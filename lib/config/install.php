950d246a30c1f4606de6c4484df6c98fe4107cf6
Serge Rodovnichenko <sergerod@gmail.com>
2014-04-01 03:36:52 +0400

    Данные в БД и магия добавления данных при установке

---
+++ lib/config/install.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package Zipomagic.Config
+ * @version 1.0
+ * @copyright (c) 2014, Serge Rodovnichenko
+ * @license http://www.webasyst.com/terms/#eula Webasyst
+ */
+waAppConfig::executeSQL(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'shop_zipomagic_zip.sql');
