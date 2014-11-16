93abe9827561b5bd770c26c2c3596bbb2115cf8f
Serge Rodovnichenko <sergerod@gmail.com>
2014-11-16 05:18:51 +0300

    Version number increase

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -10,7 +10,7 @@ return array(
     'name' => _wp('Russian Postcodes'),
     'description' => _wp('Automatically populate the Region and the City based on postcode. Russian Federation only.'),
     'vendor'=>670917,
-    'version'=>'1.0',
+    'version'=>'2.0',
     'img'=>'img/zipomagic.png',
     'shop_settings' => FALSE,
     'frontend'    => true,
