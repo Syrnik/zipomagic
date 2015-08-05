3732b154b7ecae050644d7c2ec985f6861d234b0
Serge Rodovnichenko <sergerod@gmail.com>
2015-08-05 18:00:49 +0300

    Version increase

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Zipomagic.Config
- * @version 2.1.3
+ * @version 2.1.4
  * @copyright (c) 2014, Serge Rodovnichenko
  * @license http://www.webasyst.com/terms/#eula Webasyst
  */
@@ -10,7 +10,7 @@ return array(
     'name' => _wp('Russian Postcodes'),
     'description' => _wp('Automatically populate the Region and the City based on postcode. Russian Federation only.'),
     'vendor'=>670917,
-    'version'=>'2.1.3',
+    'version'=>'2.1.4',
     'img'=>'img/zipomagic.png',
     'shop_settings' => FALSE,
     'frontend'    => true,
